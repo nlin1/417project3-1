a5ca70fbcab43995430bc0e80780cd2625f18276
Andrew J Pachulski <andrew.pachulski@technetenterprises.com>
2017-09-22 00:20:18 -0400

    added starter files

---
+++ node.rb
@@ -0,0 +1,104 @@
+$port = nil
+$hostname = nil
+
+
+
+# --------------------- Part 0 --------------------- # 
+
+def edgeb(cmd)
+	STDOUT.puts "EDGE: not implemented"
+end
+
+def dumptable(cmd)
+	STDOUT.puts "DUMPTABLE: not implemented"
+end
+
+def shutdown(cmd)
+	STDOUT.puts "SHUTDOWN: not implemented"
+	exit(0)
+end
+
+
+
+# --------------------- Part 1 --------------------- # 
+def edged(cmd)
+	STDOUT.puts "EDGED: not implemented"
+end
+
+def edgeu(cmd)
+	STDOUT.puts "EDGEu: not implemented"
+end
+
+def status()
+	STDOUT.puts "STATUS: not implemented"
+end
+
+
+# --------------------- Part 2 --------------------- # 
+def sendmsg(cmd)
+	STDOUT.puts "SENDMSG: not implemented"
+end
+
+def ping(cmd)
+	STDOUT.puts "PING: not implemented"
+end
+
+def traceroute(cmd)
+	STDOUT.puts "TRACEROUTE: not implemented"
+end
+
+def ftp(cmd)
+	STDOUT.puts "FTP: not implemented"
+end
+
+# --------------------- Part 3 --------------------- # 
+def circuit(cmd)
+	STDOUT.puts "CIRCUIT: not implemented"
+end
+
+
+
+
+# do main loop here.... 
+def main()
+
+	while(line = STDIN.gets())
+		line = line.strip()
+		arr = line.split(' ')
+		cmd = arr[0]
+		args = arr[1..-1]
+		case cmd
+		when "EDGEB"; edgeb(args)
+		when "EDGED"; edged(args)
+		when "EDGEU”; edgeU(args)
+		when "DUMPTABLE"; dumptable(args)
+		when "SHUTDOWN"; shutdown(args)
+		when "STATUS"; status()
+		when "SENDMSG"; sendmsg(args)
+		when "PING"; ping(args)
+		when "TRACEROUTE"; traceroute(args)
+		else STDERR.puts "ERROR: INVALID COMMAND \"#{cmd}\""
+		end
+	end
+
+end
+
+def setup(hostname, port, nodes, config)
+	$hostname = hostname
+	$port = port
+
+	#set up ports, server, buffers
+	
+	$socketToNode = {} #Hashmap to index node by socket
+	
+
+	main()
+
+end
+
+setup(ARGV[0], ARGV[1], ARGV[2], ARGV[3])
+
+
+
+
+
